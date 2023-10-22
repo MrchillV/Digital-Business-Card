c05436b31d75f4c6d93aaac2d32dad98d0b5c294
MrchillV <jaylonvick16@gmail.com>
2023-10-22 02:32:46 -0400

    Update About.js

--- components/About.js
+++ components/About.js
@@ -4,7 +4,7 @@ export default function About(){
     return (
         <section className="about">
             <h1>About</h1>
-            <p>I am a Student who is going to graduate in the Fall of 2023 and have a passion for web development. I was in a web development program called Global Tech Experience that helped me find my passion for web development.</p>
+            <p>I am a Student who has a passion for web development. I was in a web development program called Global Tech Experience that helped me find my passion for web development.</p>
         </section>
     );
 }
