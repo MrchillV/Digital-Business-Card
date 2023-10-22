6638bbe1194d5ba0c7a25890add372cc83f18c5c
MrchillV <jaylonvick16@gmail.com>
2023-10-22 02:20:20 -0400

    Update href

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Footer() {
     return (
         <footer className="social-media">
-            <a href="www.linkedin.com/in/jaylon-vicks" target="_blank">
+            <a href="https://www.linkedin.com/in/jaylon-vicks/" target="_blank">
                 <i class="fa-brands fa-linkedin"></i>
             </a>
             <a href="https://github.com/MrchillV" target="_blank">
