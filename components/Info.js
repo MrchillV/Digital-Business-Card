e5c1987958b480b118caab215f62d78347faeef4
MrchillV <jaylonvick16@gmail.com>
2023-10-22 02:04:18 -0400

    Updated src for img tag

--- components/Info.js
+++ components/Info.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Info() {
     return (
         <header>
-            <img src="./images/Professional.png" alt="Jaylon Vicks" />
+            <img src="../Professional.jpg" alt="Jaylon Vicks" />
             <h1 className="name">Jaylon Vicks</h1>
             <p className="title">Aspiring Frontend Developer</p>
             <a href="mailto:Jaylonvick16@gmail.com" className="email"></a>
